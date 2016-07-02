0494e0ff01f93e3544c2f27efbd0718be1be41e5
DoooReyn <jl88744653@gmail.com>
2016-07-02 14:55:32 +0800

    preload

---
+++ framework/preload.lua
@@ -0,0 +1,19 @@
+---------------------------------
+-- Author: Reyn
+-- Date: 2016-07-01
+-- Comment: Loading modules
+---------------------------------
+
+require('framework.project')
+
+require('framework.native.tableExt')
+require('framework.native.mathExt')
+require('framework.native.stringExt')
+require('framework.native.dateExt')
+
+require('framework.utils.utils')
+require('framework.utils.array')
+require('framework.utils.map')
+
+require('framework.oop.class')
+require('framework.oop.singleton')
