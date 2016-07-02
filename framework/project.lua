30361e605421a08b8a59a298be8a7521b7a8b425
DoooReyn <jl88744653@gmail.com>
2016-07-02 14:54:14 +0800

    project settings

---
+++ framework/project.lua
@@ -0,0 +1,13 @@
+---------------------------------
+-- Author: Reyn
+-- Date: 2016-07-01
+-- Comment: Project Settings
+---------------------------------
+
+local __PROJECT__ = {
+    debug = true
+}
+
+function isDebugEnv()
+    return __PROJECT__.debug
+end
